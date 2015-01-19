4318e080a5cbdf9048711940efbc63694700f14f
sebastian <sebastian.goettfert@gmx.at>
2015-01-19 15:22:04 +0100

    introduced 'duration' and 'fee' as local fields; changed correspondent methods;

--- W3Airline/src/at/jku/ce/airline/business/FlightCombo.java
+++ W3Airline/src/at/jku/ce/airline/business/FlightCombo.java
@@ -8,57 +8,42 @@ public class FlightCombo {
 
     private Flight flight1;
     private Flight flight2;
+    
+    private int duration;
+    private float fee;
 	
+    /**
+     * Constructor for DIRECT flight
+     * @param flight: single flight
+     */
     public FlightCombo(Flight flight) {
     	this.flight1 = flight;
     	this.flight2 = null;
+    	
+    	duration = calcDuration();
+    	fee = calcFee();
 	}
     
+    /**
+     * Constructor for ONE-STOP flight
+     * @param flight1: first flight
+     * @param flight2: subsequent flight
+     */
     public FlightCombo(Flight flight1, Flight flight2) {
     	this.flight1 = flight1;
     	this.flight2 = flight2;
+    	
+    	duration = calcDuration();
+    	fee = calcFee();
 	}
     
+    /* Helper Methods */
     
-	public Time getArrivalTime() {
-		if(flight2 != null)
-			return flight2.getArrivalTime();
-		else
-			return flight1.getArrivalTime();
-	}
-	public Airport getArrivesAt() {
-		if(flight2 != null)
-			return flight2.getArrivesAt();
-		else
-			return flight1.getArrivesAt();
-	}
-	public Airport getDepartesFrom() {
-		return flight1.getDepartesFrom();
-	}
-	public Time getDepartureTime() {
-		return flight1.getDepartureTime();
-	}
-	public int getStops() {
-		return flight2 == null ? 0 : 1;
-	}
-	public float getFee() {
-		
-		//TODO calculate fee
-		
-//		return 0.0f;
-		
-		return 12345.6789f;
-	}
-	public String getFirstId() {
-		return flight1.getFlightId();
-	}
-	public String getSecondId() {
-		if(flight2 == null)
-			return "";
-		else
-			return flight2.getFlightId();
-	}
-	public int getDuration() {
+    /**
+     * Calculates the duration for the flight
+     * @return duration for flight
+     */
+    private int calcDuration() {
 		int time = 0;
 		
 		int hours1;
@@ -88,6 +73,57 @@ public class FlightCombo {
 		}
 		
 		return time;
+	}
+    
+    /**
+     * Calculates the fee for the given flight
+     * @return fee for flight
+     */
+	private float calcFee() {
+
+		//TODO implement logic!!
+		
+		return 1234.56f;
+	}
+    
+    /* Getter Methods*/
+	
+	public Time getArrivalTime() {
+		if(flight2 != null)
+			return flight2.getArrivalTime();
+		else
+			return flight1.getArrivalTime();
+	}
+	public Airport getArrivesAt() {
+		if(flight2 != null)
+			return flight2.getArrivesAt();
+		else
+			return flight1.getArrivesAt();
+	}
+	public Airport getDepartesFrom() {
+		return flight1.getDepartesFrom();
+	}
+	public Time getDepartureTime() {
+		return flight1.getDepartureTime();
+	}
+	public int getStops() {
+		return flight2 == null ? 0 : 1;
+	}
+
+	public String getFirstId() {
+		return flight1.getFlightId();
+	}
+	public String getSecondId() {
+		if(flight2 == null)
+			return "";
+		else
+			return flight2.getFlightId();
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public float getFee() {
+		return fee;
 	}
 	
 }
